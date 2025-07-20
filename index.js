f4bbd549bde08f33b44364db4d0166fa388f0e3d
tclight <tclight@gmail.com>
2025-07-20 15:20:13 +0800

    Add /meme command

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
 
-const app = express();
-const port = process.env.PORT || 10000;
-
-// 你的真實 token 👇
 const token = '7524117562:AAHUW-QLrXkaXeIEkw-L5zxmiDc70V-Fn1o';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, '你好！我是 King of Meme Bot 🤖');
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "你好！我是 King of Meme Bot 🤖");
 });
+
+bot.onText(/\/meme/, (msg) => {
+  const memeUrl = 'https://i.imgur.com/W3Wq0jY.jpg';  // 這裡是隨便找的一張迷因圖，你可換成自己想要的
+  bot.sendPhoto(msg.chat.id, memeUrl, { caption: "來張迷因圖給你 😂" });
+});
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('King of Meme Bot is running!');
 });
 
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Express server is listening on port ${port}`);
 });
