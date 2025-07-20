c00c7c09a1edfd351877dc99d3c70ef8db530c25
tclight <tclight@gmail.com>
2025-07-20 15:34:31 +0800

    Add Meme image reply feature

--- index.js
+++ index.js
@@ -4,22 +4,36 @@ const express = require('express');
 const token = '7524117562:AAHUW-QLrXkaXeIEkw-L5zxmiDc70V-Fn1o';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "你好！我是 King of Meme Bot 🤖");
-});
-
-bot.onText(/\/meme/, (msg) => {
-  const memeUrl = 'https://i.imgur.com/W3Wq0jY.jpg';  // 這裡是隨便找的一張迷因圖，你可換成自己想要的
-  bot.sendPhoto(msg.chat.id, memeUrl, { caption: "來張迷因圖給你 😂" });
-});
-
+// Express server (Render 用)
 const app = express();
+const port = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
   res.send('King of Meme Bot is running!');
 });
 
-const port = process.env.PORT || 10000;
 app.listen(port, () => {
-  console.log(`Express server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
+});
+
+// /start 指令
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, '你好！我是 King of Meme Bot 🤖');
+});
+
+// 文字訊息 "Meme"
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text.toLowerCase();
+
+  if (text === 'meme') {
+    // 這裡用現成的 meme URL，你可以改成你自己的 meme 圖片庫
+    const memeUrls = [
+      'https://i.imgur.com/8fFM4Rk.jpeg',
+      'https://i.imgur.com/YOe2cUg.jpeg',
+      'https://i.imgur.com/w3duR07.png'
+    ];
+    const randomMeme = memeUrls[Math.floor(Math.random() * memeUrls.length)];
+    bot.sendPhoto(chatId, randomMeme);
+  }
 });
